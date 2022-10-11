9e10dfd8571827890805cb17dc6cd28376a52d49
Erin <etruman2@gmail.com>
2022-10-11 16:03:55 -0500

    Updated PUT and GET methods to fix 500 error

--- pages/api/speakers/[id].js
+++ pages/api/speakers/[id].js
@@ -1,7 +1,5 @@
 //import speakerData from '../../../src/SpeakerData';
 
-//import speakerData from '../../../src/SpeakerData';
-
 import path from 'path';
 import fs from 'fs';
 
@@ -10,29 +8,30 @@ const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-export default async function handler(request, response) {
-    const method = request?.method;
-    const id = parseInt(request?.query.id);
-    const recordFromBody = request?.body;
+export default async function handler(req, res) {
+    const method = req?.method; // pulling out from request parameters the method passed in
+    const id = parseInt(req?.query.id); // query method id
+    const recordFromBody = req?.body; // body of post. passed in record value to update
+
     if (method != 'PUT') {
-        response.send(501).send(`Method ${method} not implemented`);
+        res.send(501).send(`Method ${method} not implemented`);
     } else {
-        const jsonFile = path.ponseolve('./', 'db.json');
+        const jsonFile = path.resolve('./', 'db.json');
         try {
             const readFileData = await readFile(jsonFile);
             await delay(1000);
             const speakers = JSON.parse(readFileData).speakers;
             if (!speakers) {
-                response.status(404).send('Error: Request failed with status code 404');
+                res.status(404).send('Error: Request failed with status code 404');
             } else {
-                const newSpeakersArray = speakers.map(function (record) {
-                    return record.id === id ? recordFromBody : record;
+                const newSpeakersArray = speakers.map(function (rec) { // finding the record that needs to be updated
+                    return rec.id === id ? recordFromBody : rec; // replace that one with the one sent back to us with th request
                 });
                 writeFile(
                     jsonFile,
                     JSON.stringify({ speakers: newSpeakersArray }, null, 2),
                 );
-                response.status(200).json(recordFromBody);
+                res.status(200).json(recordFromBody); // return updated record. a PUT needs a response
                 console.log(`PUT /api/speakers/${id} status: 200`);
             }
         } catch (e) {
@@ -41,61 +40,5 @@ export default async function handler(request, response) {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import path from 'path';
-// import fs from 'fs';
-// import next from 'next';
-
-// const { promisify } = require('util');
-// const readFile = promisify(fs.readFile);
-// const writeFile = promisify(fs.writeFile);
-// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-// export default async function handler(req, res) {
-//     const method = req?.method; // pulling out from request parameters the method passed in
-//     const id = parseInt(req?.query.id); // query method id
-//     const recordFromBody = req?.body; // body of post. passed in record value to update
-
-//     if (method != 'PUT') { // if request is not a put, return an error
-//         res.send(501).send(`Method ${method} not implemented`);
-//     } else { // reading the speakers is the same as a GET
-//         const jsonFile = path.resolve('./', 'db.json');
-//         try {
-//             const readFileData = await readFile(jsonFile);
-//             await delay(1000);
-//             const speakers = JSON.parse(readFileData).speakers;
-//             if (!speakers) {
-//                 res.status(404).send('Error: Request failed with status code 404');
-//             } else {
-//                 const newSpeakersArray = speakers.map(function (rec) { // finding the record that needs to be updated
-//                     return rec.id === id ? recordFromBody : rec; // replace that one with the one sent back to us with th request
-//                 });
-//                 writeFile( // write new records updated back to file system
-//                     jsonFile,
-//                     JSON.stringify({ speakers: newSpeakersArray }, null, 2),
-//                 );
-//                 res.status(200).json(recordFromBody); // return updated record. a PUT needs a response
-//                 console.log(`PUT /api/speakers/${id} status: 200`);
-//             }
-//         } catch (e) {
-//             console.log(`/api/speakers/${id} error:`, e);
-//         }
-//     }
-// }
 
 // //convention for next.js api routing is to create route file name with [id] in square brackets. all requests coming from speakers/id get routed through this file. a put request would have to have an id
--- pages/api/speakers/index.js
+++ pages/api/speakers/index.js
@@ -1,4 +1,6 @@
-//  
+
+//import speakerData from '../../../src/SpeakerData';
+
 import path from 'path';
 import fs from 'fs';
 
@@ -6,31 +8,31 @@ const { promisify } = require('util');
 const readFile = promisify(fs.readFile);
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms)); // arificiallt delaying get response to make it seem more real world. for demo purposes only. also gives loading message chance to show
 
-export default async function handler(request, response) {
-    // response.status(200).send(JSON.stringify(speakerData, null, 2)) // this creates a JSON file at http://localhost:3000/api/speakers. don't need to get speakers from here if have db.json
-    const jsonFile = path.resolve('./', db.json);
+export default async function handler(req, res) {
+    //res.status(200).send(JSON.stringify(speakerData,null,2));
+
+    const jsonFile = path.resolve('./', 'db.json');
     try {
         const readFileData = await readFile(jsonFile);
-        await delay(1000)
+        await delay(1000);
         const speakers = JSON.parse(readFileData).speakers;
-        if(!speakers) {
-            response.status(404).send('Error: Request failed with status code 404')
+        if (!speakers) {
+            res.status(404).send('Error: Request failed with status code 404');
         } else {
-            response.setHeader('Content-Type', "application/json")
-            response.status(200).send(JSON.stringify(speakers,null,2))
-            console.log('GET api/speakers status: 200')
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).send(JSON.stringify(speakers, null, 2)); // this creates a JSON file at http://localhost:3000/api/speakers. don't need to get speakers from here 
+            console.log('GET /api/speakers  status: 200');
         }
     } catch (e) {
-console.log('/api/speakers error:', e) // just writing errors to console if fails
+        console.log('/api/speakers error:', e);// just writing errors to console if fails
     }
 }
+
 
 // build REST web service that implements GET and PUT
 //follow next.js api routes following their conventions
 // convention to have api folder in pages folder
 //incluse javascript that gets run when user sends a request tho that url by name 9url name the same as name of folder, that is)
 // this is for speaker get request
-// 
 
-//import speakerData from '../../../src/SpeakerData';
 

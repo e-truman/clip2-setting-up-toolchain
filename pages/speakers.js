d8e4b4b647957aaa501e0949d5daad9e10a4608f
Erin <etruman2@gmail.com>
2022-10-14 10:32:29 -0500

    Enabled incremental static regeneration

--- pages/speakers.js
+++ pages/speakers.js
@@ -1,10 +1,38 @@
-import React from 'react';
 import App from '../src/App';
+import path from 'path';
+import fs from 'fs';
+import React from 'react';
 
-function speakers() {
-  return <App pageName="Speakers" />;
+export const InitialSpeakersDataContext = React.createContext();
+
+export async function getStaticProps() { //
+  const { promisify } = require('util');
+  const readFile = promisify(fs.readFile); //reference to readfile comes from reference to promisify
+  const jsonFile = path.resolve('./', 'db.json'); //path to json filr
+  let initialSpeakersData;
+  try {
+    const readFileData = await readFile(jsonFile);
+    initialSpeakersData = JSON.parse(readFileData).speakers; // parse it in to js array
+  } catch (e) {
+    console.log('/api/speakers error:', e); // if error log error
+  }
+
+  return { revalidate: 1, props: { initialSpeakersData } }; // return initial speaker data as property of props
+}
+
+function speakers({ initialSpeakersData }) { //we can now access as a receiving prop in client sides
+  return(
+    <InitialSpeakersDataContext.Provider value={initialSpeakersData}>
+      <App pageName="Speakers" />
+    </InitialSpeakersDataContext.Provider>
+  );
 }
 
 export default speakers;
 
-// 
+
+
+// // special exported functiond inside of pages will run exclusively on the Node server
+// // what's returned from exported functions is serialized and passed to client components as receiving props
+// //getServerSideProps is meant to resolve all external services that provide data for current page route
+// // DB.json simulates external REST service on our Node server
--- src/useSpeakerDataManager.js
+++ src/useSpeakerDataManager.js
@@ -1,13 +1,17 @@
-import speakersReducer from "./speakersReducer";
-// import SpeakerData from "./SpeakerData";
-import { useEffect, useReducer } from "react";
+import speakersReducer from './speakersReducer';
 import axios from 'axios';
+import { useEffect, useReducer, useContext } from 'react';
+
+import { InitialSpeakersDataContext } from '../pages/speakers';
 
 const useSpeakerDataManager = () => {
+    const initialSpeakersData = useContext(InitialSpeakersDataContext);
     // const [speakerList, setSpeakerList] = useState([]); // use reducer below accomplishes the same thing as this
     const [{ isLoading, speakerList }, dispatch] = useReducer(speakersReducer, {
-        isLoading: true,
+        isLoading:true,
         speakerList: [],
+        // isLoading: false,// no loading when you use server side rendering and useContext to initialize the array
+        // speakerList: initialSpeakersData, // 
     })
     // first parameter is reducer function, second parameter is what ti initialize oor state to
     // const isLoading = stateObject.isLoading
@@ -18,49 +22,53 @@ const useSpeakerDataManager = () => {
     // when use dispatch keyword, speakersReducer is called. changed to dispatch from setSpeakerList
 
     const toggleSpeakerFavorite = (speakerRecord) => {
-        const updateData = async function(){
-            axios.put(`/api/speakers/${speakerRecord.id}`, {...speakerRecord, favorite: !speakerRecord.favorite})
-        speakerRecord.favorite === true ?
-            dispatch({ type: "unfavorite", id: speakerRecord.id }) :
-            dispatch({ type: "favorite", id: speakerRecord.id })
+        const updateData = async function () {
+            axios.put(`/api/speakers/${speakerRecord.id}`, { ...speakerRecord, favorite: !speakerRecord.favorite })
+            speakerRecord.favorite === true ?
+                dispatch({ type: "unfavorite", id: speakerRecord.id }) :
+                dispatch({ type: "favorite", id: speakerRecord.id })
         };
         updateData();
     }
 
-    useEffect(() => {
-        // setIsLoading(true); // makes sure loading status set to true // can remove from code after including isLoading state with reducer
-        // new Promise(function (resolve) {
-        //     setTimeout(function () {
-        //         resolve();
-        //     }, 1000); // reates one second delay, returns a promise
-        // }).then(() => {
-        //     // setIsLoading(false)
-        //     // const speakerListServerFilter = SpeakerData.filter(({ sat, sun }) => { // use reducer filters now
-        //     //     return (speakingSaturday && sat) || (speakingSunday && sun);
-        //     // });
-        //     // setSpeakerList(speakerListServerFilter); // after loads, sets speaker list to speaker data
-        //     // setIsLoading(false); // sets loading to false
 
-        //     dispatch({
-        //         type: "setSpeakerList",
-        //         data: SpeakerData
-        //     })
-        // });
+useEffect(() => {
+    // setIsLoading(true); // makes sure loading status set to true // can remove from code after including isLoading state with reducer
+    // new Promise(function (resolve) {
+    //     setTimeout(function () {
+    //         resolve();
+    //     }, 1000); // reates one second delay, returns a promise
+    // }).then(() => {
+    //     // setIsLoading(false)
+    //     // const speakerListServerFilter = SpeakerData.filter(({ sat, sun }) => { // use reducer filters now
+    //     //     return (speakingSaturday && sat) || (speakingSunday && sun);
+    //     // });
+    //     // setSpeakerList(speakerListServerFilter); // after loads, sets speaker list to speaker data
+    //     // setIsLoading(false); // sets loading to false
 
-        // don't need our timer if using axios
-        const fetchData = async function() { // modifying our hook to get data from REST service
-            let result = await axios.get('/api/speakers'); //rest get url
-            dispatch({ type: 'setSpeakerList', data: result.data })
-        }
-        fetchData();
-        return () => {
-            console.log('cleanup'); // cleanup function would go here, we don't need that
-        };
-    }, []); // only runs once
-    // return { isLoading, speakerList, dispatch };
-    return { isLoading, speakerList, toggleSpeakerFavorite }; // handled dispatch in toggleSpeakerFavorite function to make code easier to read. hard to understand what dispatch means
+    //     dispatch({
+    //         type: "setSpeakerList",
+    //         data: SpeakerData
+    //     })
+    // });
+
+    // don't need our timer if using axios
+    const fetchData = async function () { // modifying our hook to get data from REST service
+        let result = await axios.get('/api/speakers'); //rest get url
+        dispatch({ type: 'setSpeakerList', data: result.data })
+    }
+    fetchData();
+    return () => {
+        console.log('cleanup'); // cleanup function would go here, we don't need that
+    };
+}, []); // only runs once
+// return { isLoading, speakerList, dispatch };
+return { isLoading, speakerList, toggleSpeakerFavorite }; // handled dispatch in toggleSpeakerFavorite function to make code easier to read. hard to understand what dispatch means
 }
 
 export default useSpeakerDataManager
 
 // npm install axios
+// server side rendering good fro SEO because crawlers guaranteed to see all crawlers on your page
+// loads faster becasue user doesn't have to wait for use effect to run to see data
+// possibility of overloading server is downside
